f82e7b2371a8eb164f7fff96230569c137cba39d
DISHA JINDAL <djjindal@park.key.seas.upenn.edu>
2019-10-05 16:34:44 -0400

    Init Project

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file      main.cpp
+ * @brief     Scan Matching
+ * @authors   Disha Jindal
+ * @date      2019
+ * @copyright University of Pennsylvania
+ */
+#include <iostream>
+#include <cstdio>
+#include <scan_matching/icp.h>
+#include <scan_matching/common.h>
+#include "testing_helpers.hpp"
+#include <fstream>
+#include <iostream>
+#include <string.h>
+#include <sstream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	cout << "In Scan Matching Main\n";
+}
